23b19e04cbfddab20bbad98d801e55765e586c2d
趣味的吃 <2364319479@qq.com>
2023-04-09 13:35:23 +0800

    pyrdW基本逻辑实现，仍未完成，等待后续更新

--- pyrdw.py
+++ pyrdw.py
@@ -1,5 +1,6 @@
 # This Python file uses the following encoding: utf-8
 
+import inspect
 import sys
 
 
@@ -14,6 +15,7 @@ def listAllEditableFunctions():
     functions.remove("SetLevelMeta")
     functions.remove("parseIf")
     functions.remove("AddCharacter")
+    functions.remove("Export")
     return functions
 
 class PyRD(QMainWindow):
@@ -24,19 +26,82 @@ class PyRD(QMainWindow):
         self.ui = Ui_PyRD()
         self.ui.setupUi(self)
         self.ui.importFile.triggered.connect(self.select_file) # 当按下self.ui.importFile,执行select_file函数
+        self._lastPara=""
+        self.id = 0
+        self.sprdDict={
+            "metadata":{
+            
+            },
+            "charcaters":{
+            
+            },
+            "bars":{
+            
+            }
+        }
+        self.curBarList=[]
+        self.curParaDict={}
         #在self.ui.eventList里输出所有pyrd支持的函数,调用pyrd.__all__
         self.ui.eventList.addItems(listAllEditableFunctions())
-        self.file = None
-        #self.ui.exportFile.triggered.connect(self.export_file)
-        #self.ui.compile.triggered.connect(self.compile_file)
-        #self.ui.compileSave.triggered.connect(self.compile_save_file)
-        #self.ui.clearEvent.triggered.connect(self.clear_event)
-        #self.ui.metadataSetEvent.triggered.connect(self.metadata_set_event)
-        #self.ui.characterSetEvent.triggered.connect(self.character_set_event)
-        #self.ui.delEvent.clicked.connect(self.del_event)
-        #self.ui.addEvent.clicked.connect(self.add_event)
-        
+        #在点击self.ui.eventList的一个项目的时候,使用inspect模块在self.ui.selectPara里展示
+        self.ui.eventList.itemClicked.connect(self.showPara)
+        self.ui.barEventList.itemClicked.connect(self.showBarPara)
+        self.ui.selectPara.itemClicked.connect(self.saveAndRefreshPara)
+        self.ui.addEvent.clicked.connect(self.addEvent)
 
+    def saveAndRefreshPara(self):
+        if not self._lastPara:
+            self._lastPara = self.ui.selectPara.currentItem().text()
+            return
+        if self._lastPara == self.ui.selectPara.currentItem().text():
+            return
+        Arg=self.ui.eventEdit.text()
+        if Arg:
+            self.curParaDict[self._lastPara] = eval(Arg)
+        if self.ui.selectPara.currentItem().text() in self.curParaDict:
+            self.ui.eventEdit.setText(str(self.curParaDict[self.ui.selectPara.currentItem().text()]))
+        else:
+            self.ui.eventEdit.clear()
+        self._lastPara = self.ui.selectPara.currentItem().text()
+        print("curParaDict:",self.curParaDict)
+
+    def showBarPara(self):
+        #获取eventList点击的项目,并且使用inspect获取对应pyrd库函数的所有参数
+        self.ui.selectPara.clear()
+        self.ui.eventEdit.clear()
+        selectedEvent=self.ui.barEventList.currentItem().text()
+        para=inspect.getfullargspec(eval("pyrd."+selectedEvent)).args
+        para.remove("bar")
+        #清除lastPara与curParaDict
+        self._lastPara = ""
+        #self.curParaDict = self.curBarList
+        for curParaDict in self.curBarList:
+            if curParaDict["id"] == self.ui.barEventList.currentIndex():
+                self.curParaDict = curParaDict
+        #在self.ui.selectPara里展示参数
+        self.ui.selectPara.addItems(para)
+
+    def showPara(self):
+        #获取eventList点击的项目,并且使用inspect获取对应pyrd库函数的所有参数
+        self.ui.selectPara.clear()
+        self.ui.eventEdit.clear()
+        selectedEvent=self.ui.eventList.currentItem().text()
+        para=inspect.getfullargspec(eval("pyrd."+selectedEvent)).args
+        para.remove("bar")
+        #清除lastPara与curParaDict
+        self._lastPara = ""
+        self.curParaDict = {"func":selectedEvent,"id":self.id}
+        #在self.ui.selectPara里展示参数
+        self.ui.selectPara.addItems(para)
+
+    def addEvent(self):
+        self.saveAndRefreshPara()
+        self.curBarList.append(self.curParaDict)
+        self.ui.barEventList.clear()
+        for function in self.curBarList:
+            self.ui.barEventList.addItem(function["func"])
+        self.id = self.id+1
+        print("curBarList:",self.curBarList)
 
     def select_file(self):
         file_path = QFileDialog.getOpenFileName(self,caption="请选择sprd文件", filter="(*.sprd)")
